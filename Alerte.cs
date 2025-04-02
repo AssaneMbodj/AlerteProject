752a83acc8b51ec6d1e2eb1148e29a7d69e61400
satou032 <165910258+satou032@users.noreply.github.com>
2025-04-03 01:29:38 +0200

    models

---
+++ Alerte.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApp.Models
+{
+    public class Alerte
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Type { get; set; } // Agression, incendie, etc.
+        public string Description { get; set; }
+        public string Lieu { get; set; }
+        public string NiveauDanger { get; set; } // Faible, moyen, élevé
+        public DateTime DateCreation { get; set; } = DateTime.Now;
+        public string UtilisateurId { get; set; }
+       
+    }
+}
---
+++ Utilisateur.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public class Utilisateur
+    {
+        public int id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Role { get; set; } // Utilisateur ou Autorité
+    }
+}
