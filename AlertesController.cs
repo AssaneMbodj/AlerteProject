ca3fc71895e8f8493deefd0858332d6c15dae094
satou032 <165910258+satou032@users.noreply.github.com>
2025-04-03 01:30:18 +0200

    Controllers

---
+++ AlertesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class AlertesController : Controller
+    {
+        private readonly WebAppContext _context;
+
+        public AlertesController(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Alertes
+        [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Alerte.ToListAsync());
+        }
+
+        // GET: Alertes/Details/5
+        [Authorize]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alerte = await _context.Alerte
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alerte == null)
+            {
+                return NotFound();
+            }
+
+            return View(alerte);
+        }
+
+        // GET: Alertes/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alertes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Create([Bind("Id,Titre,Type,Description,Lieu,NiveauDanger,DateCreation,UtilisateurId")] Alerte alerte)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _context.Add(alerte);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alerte);
+        }
+
+        // GET: Alertes/Edit/5
+        [Authorize]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alerte = await _context.Alerte.FindAsync(id);
+            if (alerte == null)
+            {
+                return NotFound();
+            }
+            return View(alerte);
+        }
+
+        // POST: Alertes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Titre,Type,Description,Lieu,NiveauDanger,DateCreation,UtilisateurId")] Alerte alerte)
+        {
+            if (id != alerte.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(alerte);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlerteExists(alerte.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alerte);
+        }
+
+        // GET: Alertes/Delete/5
+        [Authorize]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alerte = await _context.Alerte
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (alerte == null)
+            {
+                return NotFound();
+            }
+
+            return View(alerte);
+        }
+
+        // POST: Alertes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var alerte = await _context.Alerte.FindAsync(id);
+            if (alerte != null)
+            {
+                _context.Alerte.Remove(alerte);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AlerteExists(int id)
+        {
+            return _context.Alerte.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ UtilisateursController.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Data;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class UtilisateursController : Controller
+    {
+        private readonly WebAppContext _context;
+
+        public UtilisateursController(WebAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Utilisateurs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Utilisateur.ToListAsync());
+        }
+        
+        // GET: Utilisateurs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilisateur = await _context.Utilisateur
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Create
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+        // POST: Utilisateurs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        
+        public async Task<IActionResult> Create([Bind("id,Nom,Prenom,Role")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(utilisateur);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilisateur);
+        }
+       
+        // GET: Utilisateurs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilisateur = await _context.Utilisateur.FindAsync(id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+       
+        public async Task<IActionResult> Edit(int id, [Bind("id,Nom,Prenom,Role")] Utilisateur utilisateur)
+        {
+            if (id != utilisateur.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(utilisateur);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UtilisateurExists(utilisateur.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Delete/5
+        
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilisateur = await _context.Utilisateur
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (utilisateur == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilisateur);
+        }
+
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login";
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model) 
+        {
+            ViewBag.Title = "Login";
+            if (ModelState.IsValid)
+            {
+                var utilisateurs = _context.Utilisateur.Where(c => c.Prenom == model.UserName).FirstOrDefault();
+                if(utilisateurs == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt");
+                    return View(model);
+                }
+                //here we add cookie to users
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, utilisateurs.Prenom),
+                    new Claim("Name", utilisateurs.Nom),
+                    new Claim(ClaimTypes.Role, "User"),
+                    new Claim(ClaimTypes.NameIdentifier, utilisateurs.id.ToString())
+                };
+
+                var authProperties = new AuthenticationProperties
+                {
+                    AllowRefresh = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(5),
+                    IsPersistent = true,
+                    IssuedUtc = DateTime.UtcNow,
+                    //RedirectUri = <string>,
+                };
+
+                var claimIdentity = new ClaimsIdentity(claims,
+                    CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimIdentity),
+                    authProperties);
+
+                return RedirectToAction("Index","AffichageAlertes");
+            
+            }
+            return View(model);
+        }
+
+        // POST: Utilisateurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var utilisateur = await _context.Utilisateur.FindAsync(id);
+            if (utilisateur != null)
+            {
+                _context.Utilisateur.Remove(utilisateur);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UtilisateurExists(int id)
+        {
+            return _context.Utilisateur.Any(e => e.id == id);
+        }
+    }
+}
