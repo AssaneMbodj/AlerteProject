2c36f8e67ef7f6f7d571c0af721a5e3d768b91dc
satou032 <165910258+satou032@users.noreply.github.com>
2025-04-03 02:02:44 +0200

    Data

---
+++ WebAppContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class WebAppContext : DbContext
+    {
+        public WebAppContext(DbContextOptions<WebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApp.Models.Alerte> Alerte { get; set; } = default!;
+        public DbSet<WebApp.Models.Utilisateur> Utilisateur { get; set; } = default!;
+    }
+}
